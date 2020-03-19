38d44528e89a572592fca27b6192a78e713c6389
Max Ringelheim <max.ringelheim@gmail.com>
2020-03-19 13:19:14 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,5 +6,5 @@ def introduction_with_language (name, language)
   puts "Hi, my name is #{name} and I'm learning to program in #{language}."
 end
 
-def introduction_with_language (Dan, Emberjs)
+def introduction_with_language (dan, emberjs)
 end
